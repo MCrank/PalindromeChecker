25d89b22a7c9bdc44e898e9e3a9a53fdb5df6381
MCrank <marco_mcse@hotmail.com>
2019-03-09 11:13:10 -0600

    Checker is working

--- PalindromeChecker/Program.cs
+++ PalindromeChecker/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace PalindromeChecker
+namespace MyTestPlayground
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a String");
+            string myString = Console.ReadLine().ToLower();
+
+            var inputString = "";
+            var compareString = "";
+
+            // Cleanup our input (N punctuation, spaces, lowercase)
+            foreach (char mychar in myString)
+            {
+                if (!char.IsPunctuation(mychar))
+                {
+                    inputString += mychar;
+                }
+            }
+            inputString = inputString.Replace(" ", string.Empty);
+
+            // Reverse the input
+            compareString = ReverseString(inputString);
+
+            // Compare the two strings
+            if (compareString == inputString)
+            {
+                Console.WriteLine("You made one of those palin thingies!");
+            }
+            else
+            {
+                Console.WriteLine("You are dumb!");
+            }
+
+            Console.ReadLine();
+        }
+
+        // Method to reverse the input string goes here
+        public static string ReverseString(string s)
+        {
+            char[] array = s.ToCharArray();
+            Array.Reverse(array);
+            return new string(array);
         }
     }
 }
